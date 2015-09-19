cd4a4102aa16c25eb097f0980fad7d442e1aec62
Richard Remer <rich@zingle.me>
2015-09-18 19:04:08 -0700

    noopable function

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Decorate function or method with .enable() and .disable() methods.
+ * @param {function|object} target
+ * @param {string} [prop]
+ */
+function noopable(target, prop) {
+    var obj, fn,
+        patched,
+        disabled = false;
+
+    if (arguments.length > 1) obj = target, fn = target[prop];
+    else fn = target;
+
+    if (fn.noopable) return fn;
+
+    patched = function() {
+        if (!disabled) fn.apply(this, arguments);
+    }
+
+    patched.noopable = true;
+    patched.disable = function() {disabled = true; return patched;}
+    patched.enable = function() {disabled = false; return patched;}
+
+    if (obj) {
+        patched.restore = function() {obj[prop] = fn;}
+        obj[prop] = patched;
+    }
+
+    return obj ? patched.bind(obj) : patched;
+}
+
+/** export noopable function */
+module.exports = noopable;
---
+++ test/test-noopable.js
@@ -0,0 +1,44 @@
+var noopable = require(".."),
+    expect = require("expect.js"),
+    sinon = require("sinon");
+
+describe("noopable", function() {
+    describe("(function)", function() {
+        it("should return noopable function", function() {
+            var spy = sinon.spy(),
+                fn = noopable(spy);
+
+            expect(fn).to.be.a("function");
+            expect(fn.enable).to.be.a("function");
+            expect(fn.disable).to.be.a("function");
+            expect(fn).to.not.be(spy);
+
+            fn();
+            expect(spy.calledOnce).to.be(true);
+
+            fn.disable();
+            fn();
+            expect(spy.calledOnce).to.be(true);
+
+            fn.enable();
+            fn();
+            expect(spy.calledTwice).to.be(true);            
+        });
+    });
+
+    describe("(object, string)", function() {
+        it("should return a noopable method", function() {
+            var obj = {method: sinon.spy()},
+                spy = obj.method;
+
+            noopable(obj, "method");
+            expect(obj.method.enable).to.be.a("function");
+            expect(obj.method.disable).to.be.a("function");
+            expect(obj.method.restore).to.be.a("function");
+            expect(obj.method).to.not.be(spy);
+
+            obj.method.restore();
+            expect(obj.method).to.be(spy);
+        });
+    });
+});
